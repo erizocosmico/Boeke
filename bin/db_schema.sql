3ca06bb6d1df104a01d0ecc6f142a8a767692557
José Miguel Molina Arboledas <rd4091@gmail.com>
2013-11-16 22:57:06 +0100

    init(*): Initialized the default structure for the project

---
+++ bin/db_schema.sql
@@ -0,0 +1,138 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+-- -----------------------------------------------------
+-- Table `usuario`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `usuario` ;
+
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` VARCHAR(60) NOT NULL,
+  `nombre_usuario_limpio` VARCHAR(60) NOT NULL,
+  `usuario_pass` VARCHAR(255) NOT NULL,
+  `usuario_salt` VARCHAR(255) NOT NULL,
+  `es_admin` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  UNIQUE INDEX `nombre_usuario_UNIQUE` (`nombre_usuario` ASC),
+  UNIQUE INDEX `nombre_usuario_limpio_UNIQUE` (`nombre_usuario_limpio` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `asignatura`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `asignatura` ;
+
+CREATE TABLE IF NOT EXISTS `asignatura` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `nivel` TINYINT UNSIGNED NOT NULL,
+  `nombre` VARCHAR(60) NOT NULL,
+  PRIMARY KEY (`id`, `nombre`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `alumno`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `alumno` ;
+
+CREATE TABLE IF NOT EXISTS `alumno` (
+  `nie` BIGINT UNSIGNED NOT NULL,
+  `nombre` VARCHAR(70) NOT NULL,
+  `curso_actual` TINYINT UNSIGNED NOT NULL DEFAULT 1,
+  `repitiendo` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`nie`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `libro`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `libro` ;
+
+CREATE TABLE IF NOT EXISTS `libro` (
+  `isbn` VARCHAR(13) NOT NULL,
+  `titulo` VARCHAR(80) NOT NULL,
+  `autor` VARCHAR(85) NOT NULL,
+  `anio` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`isbn`),
+  UNIQUE INDEX `isbn_UNIQUE` (`isbn` ASC))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `libro_asignatura`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `libro_asignatura` ;
+
+CREATE TABLE IF NOT EXISTS `libro_asignatura` (
+  `libro_isbn` VARCHAR(13) NOT NULL,
+  `asignatura_id` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`libro_isbn`, `asignatura_id`),
+  INDEX `libro_asignatura_asignatura_id_idx` (`asignatura_id` ASC),
+  CONSTRAINT `libro_asignatura_libro_id`
+    FOREIGN KEY (`libro_isbn`)
+    REFERENCES `libro` (`isbn`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `libro_asignatura_asignatura_id`
+    FOREIGN KEY (`asignatura_id`)
+    REFERENCES `asignatura` (`id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ejemplar`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `ejemplar` ;
+
+CREATE TABLE IF NOT EXISTS `ejemplar` (
+  `codigo` INT UNSIGNED NOT NULL,
+  `libro_isbn` VARCHAR(13) NOT NULL,
+  `estado` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY (`codigo`),
+  INDEX `ejemplar_libro_id_fk_idx` (`libro_isbn` ASC),
+  CONSTRAINT `ejemplar_libro_id_fk`
+    FOREIGN KEY (`libro_isbn`)
+    REFERENCES `libro` (`isbn`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `historial`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `historial` ;
+
+CREATE TABLE IF NOT EXISTS `historial` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `tipo` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+  `ejemplar_codigo` INT UNSIGNED NOT NULL,
+  `alumno_nie` BIGINT UNSIGNED NULL,
+  `estado` TINYINT UNSIGNED NOT NULL DEFAULT 0,
+  `fecha` BIGINT UNSIGNED NOT NULL,
+  `anotacion` BLOB NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `historial_ejemplar_codigo_fk_idx` (`ejemplar_codigo` ASC),
+  INDEX `historial_alumno_nie_fk_idx` (`alumno_nie` ASC),
+  CONSTRAINT `historial_ejemplar_codigo_fk`
+    FOREIGN KEY (`ejemplar_codigo`)
+    REFERENCES `ejemplar` (`codigo`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `historial_alumno_nie_fk`
+    FOREIGN KEY (`alumno_nie`)
+    REFERENCES `alumno` (`nie`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;

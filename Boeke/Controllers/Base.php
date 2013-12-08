4e977649229201d398a654bb9147150283ef526a
José Miguel Molina Arboledas <rd4091@gmail.com>
2013-12-08 21:22:53 +0100

    Añadida la función de generación de paginación

--- Boeke/Controllers/Base.php
+++ Boeke/Controllers/Base.php
@@ -36,4 +36,68 @@ class Base
 {
     public static $app;
     public static $config;
+    
+    public static function generatePagination(
+        $set,
+        $perPage,
+        $current,
+        callable $urlCallback
+    ) {
+        $count = $set->count();
+        $pages = floor($count / $perPage);
+        if ($pages != $count / $perPage) {
+            $pages++;
+        }
+        
+        if ($pages < 1) {
+            return '';
+        }
+        
+        if ($pages < $current) {
+            $current = 1;
+        }
+        
+        $output = '';
+        
+        if ($current > 1) {
+            // Primera página
+            $output .= '<li><a href="' . $urlCallback(1) .
+                '">&larr; Primera</a></li>';
+        }
+        
+        if ($current < 8 || $pages < 8) {
+            // Principio
+            for ($i = 1; $i < (($pages + 1) < 8 ? $pages + 1 : 8); $i++) {
+                $output .= '<li' . ($current == $i ? ' class="active"' : '') .
+                    '><a href="' . $urlCallback($i) . '">' . $i . '</a></li>';
+            }
+            
+            if ($pages > (($pages) < 7 ? $pages : 7)) {
+                $output .= '<li class="disabled"><span>...</span></li>';
+            }
+        } elseif ($pages - $current < 8) {
+            // Final
+            $output .= '<li class="disabled"><span>...</span></li>';
+            for ($i = $pages - 7; $i <= $pages; $i++) {
+                $output .= '<li' . ($current == $i ? ' class="active"' : '') .
+                    '><a href="' . $urlCallback($i) . '">' . $i . '</a></li>';
+            }
+        } else {
+            // Mitad
+            $output .= '<li class="disabled"><span>...</span></li>';
+            for ($i = $current - 3; $i <= $current + 4; $i++) {
+                $output .= '<li' . ($current == $i ? ' class="active"' : '') .
+                    '><a href="' . $urlCallback($i) . '">' . $i . '</a></li>';
+            }
+            $output .= '<li class="disabled"><span>...</span></li>';
+        }
+        
+        if ($current < $pages) {
+            // Última página
+            $output .= '<li><a href="' . $urlCallback($pages) .
+                '">Última &rarr;</a></li>';
+        }
+        
+        return $output;
+    }
 }

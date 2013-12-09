d074c13eb3e2429c08318b932c60ed6ac1f51317
José Miguel Molina Arboledas <rd4091@gmail.com>
2013-12-09 21:13:33 +0100

    Cambio de versión a 0.1.2

--- Boeke/Models/Sesion.php
+++ Boeke/Models/Sesion.php
@@ -6,7 +6,7 @@
  * @copyright   2013 José Miguel Molina
  * @link        https://github.com/mvader/Boeke
  * @license     https://raw.github.com/mvader/Boeke/master/LICENSE
- * @version     0.1.1
+ * @version     0.1.2
  * @package     Boeke
  *
  * MIT LICENSE

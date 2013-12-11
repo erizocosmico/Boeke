55642b870e18d36d7f7086c1bfb126f98b19aaad
José Miguel Molina Arboledas <rd4091@gmail.com>
2013-12-11 23:36:20 +0100

    Cambio de versión a la 0.2.0

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@
  * @copyright   2013 José Miguel Molina
  * @link        https://github.com/mvader/Boeke
  * @license     https://raw.github.com/mvader/Boeke/master/LICENSE
- * @version     0.1.3
+ * @version     0.2.0
  * @package     Boeke
  *
  * MIT LICENSE

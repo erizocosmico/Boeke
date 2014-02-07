2834be554620e4bcb3689ae9ec49774a2a73e64c
José Miguel Molina Arboledas <rd4091@gmail.com>
2014-02-07 13:08:18 +0100

    Añadida la gestión de asignaturas

--- public/js/main.js
+++ public/js/main.js
@@ -40,7 +40,6 @@ function showLevelEditor(elem) {
     modalBox.modal('show');
     modalTitle.innerHTML = (editing) ? 'Editar nivel' : 'Nuevo nivel';
     if (editing) {
-        console.log(elem);
         name.value = elem.getAttribute('data-name');
         url += 'edit/' + elem.getAttribute('data-id');
         method = 'PUT';
@@ -53,8 +52,8 @@ function showLevelEditor(elem) {
     panel.className = '';
     actions.className = '';
     createButton.onclick = function(e) {
-        if (name.value.length < 5) {
-            displayModalAlert('El nombre del nivel debe tener al menos 5 caracteres', 'danger');
+        if (name.value.length < 3) {
+            displayModalAlert('El nombre del nivel debe tener al menos 3 caracteres', 'danger');
         } else {
             hideModalAlert();
             
@@ -67,7 +66,7 @@ function showLevelEditor(elem) {
                 name.value = '';
                 setTimeout(function() {
                     refresh();
-                }, 2000);
+                }, 1000);
             }).error(function(data) {
                 if (data.readyState === 4) {
                     displayModalAlert(JSON.parse(data.responseText).error, 'danger');
@@ -95,6 +94,129 @@ function deleteLevel(elem) {
         hideModalAlert();
         $.ajax({
             url: baseUrl + 'levels/delete/' + elem.getAttribute('data-id'),
+            data: getCsrfToken() + '&confirm=yes',
+            type: 'DELETE'
+        }).done(function(data) {
+            if (data.deleted) {
+                panel.className = 'hidden';
+                displayModalAlert(data.message, 'success');
+                setTimeout(function() {
+                    refresh();
+                }, 1000);
+            } else {
+                modalBox.modal('hide');
+            }
+        }).error(function(data) {
+            if (data.readyState === 4) {
+                panel.className = 'hidden';
+                displayModalAlert(JSON.parse(data.responseText).error, 'danger');
+            }
+        });
+    };
+    cancelButton.onclick = function(e) {
+        modalBox.modal('hide');
+    };
+}
+
+function showSubjectEditor(elem) {
+    var panel = document.getElementById('editor-panel'),
+        actions = document.getElementById('editor-actions'),
+        name = document.getElementById('nombre'),
+        level = $('#nivel'),
+        createButton = document.getElementById('create-subject'),
+        cancelButton = document.getElementById('cancel-create'),
+        editing = elem !== undefined,
+        url = 'subjects/',
+        method = 'POST';
+        
+    $level = level.selectize({
+        valueField: 'id',
+        labelField: 'name',
+        searchField: 'name',
+        preload: true,
+        openOnFocus: true,
+        create: false,
+        render: {
+            option: function(item, escape) {
+                return '<div>' + escape(item.name) + '</div>';
+            }
+        },
+        load: function(query, callback) {
+            $.ajax({
+                url: baseUrl + 'levels/all',
+                type: 'GET',
+                error: function() {
+                    callback();
+                },
+                success: function(res) {
+                    callback(res.levels);
+                    
+                    if (editing) {
+                        var sel = $level[0].selectize;
+                        sel.setValue(elem.getAttribute('data-level-id'));
+                    }
+                }
+            });
+        }
+    });
+
+    modalBox.modal('show');
+    modalTitle.innerHTML = (editing) ? 'Editar asignatura' : 'Nueva asignatura';
+    if (editing) {
+        name.value = elem.getAttribute('data-name');
+        url += 'edit/' + elem.getAttribute('data-id');
+        method = 'PUT';
+        createButton.innerHTML = 'Editar asignatura';
+    } else {
+        name.value = '';
+        url += 'new';
+        createButton.innerHTML = 'Nueva asignatura';
+    }
+    panel.className = '';
+    actions.className = '';
+    createButton.onclick = function(e) {
+        if (name.value.length < 3) {
+            displayModalAlert('El nombre de la asignatura debe tener al menos 3 caracteres', 'danger');
+        } else {
+            hideModalAlert();
+            
+            $.ajax({
+                url: baseUrl + url,
+                data: "nombre=" + name.value + '&nivel=' + level.val() +  '&' + getCsrfToken(),
+                type: method
+            }).done(function(data) {
+                displayModalAlert(data.message, 'success');
+                name.value = '';
+                setTimeout(function() {
+                    refresh();
+                }, 1000);
+            }).error(function(data) {
+                if (data.readyState === 4) {
+                    displayModalAlert(JSON.parse(data.responseText).error, 'danger');
+                }
+            });
+        }
+    };
+    cancelButton.onclick = function(e) {
+        modalBox.modal('hide');
+    };
+}
+
+function deleteSubject(elem) {
+    var panel = document.getElementById('delete-panel'),
+        actions = document.getElementById('delete-actions'),
+        deleteButton = document.getElementById('delete-subject'),
+        cancelButton = document.getElementById('cancel-delete');
+
+    modalBox.modal('show');
+    modalTitle.innerHTML = 'Borrar asignatura';
+    panel.className = '';
+    actions.className = '';
+    panel.innerHTML = '¿Deseas borrar la asignatura "<b>' + elem.getAttribute('data-name') + '</b>"?';
+    deleteButton.onclick = function(e) {
+        hideModalAlert();
+        $.ajax({
+            url: baseUrl + 'subjects/delete/' + elem.getAttribute('data-id'),
             data: getCsrfToken() + '&confirm=yes',
             type: 'DELETE'
         }).done(function(data) {
